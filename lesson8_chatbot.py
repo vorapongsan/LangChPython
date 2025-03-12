9951c899dab72772017bfe0a757546117bc2076b
vorapongsan <60056864+vorapongsan@users.noreply.github.com>
2025-03-12 06:52:38 +0000

    feat: integrate TogetherEmbeddings for enhanced document retrieval

--- lesson8_chatbot.py
+++ lesson8_chatbot.py
@@ -10,11 +10,14 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
 from langchain.chains import create_retrieval_chain
+from langchain_together import TogetherEmbeddings
 
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import MessagesPlaceholder
 
-
+embedding = TogetherEmbeddings(
+    model="togethercomputer/m2-bert-80M-8k-retrieval",
+)
 
 def  get_document_from_web(url):
     loader = WebBaseLoader(url)
@@ -30,7 +33,7 @@ def  get_document_from_web(url):
     return splitDocs
 
 def create_db(docs):
-    embedding = OpenAIEmbeddings()
+    # embedding = OpenAIEmbeddings()
     vectorStore = FAISS.from_documents(docs, embedding = embedding) 
     return vectorStore
 
